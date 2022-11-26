1237be4c69795141b7063ee8a82b872d83755bb3
Валентин <123qwemmm@mail.ru>
2022-11-26 20:35:23 +0300

    забирает data из готового сета

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def great_person(item: GreatPerson):
     table = collection.find_one({'token': item['token']}, {"_id": 1})
     collection = base['data']
     collection.update_one(table,
-                          {'$set': {str(int(time.time())): [1, 5, 100, 1]}},
+                          {'$set': {str(int(time.time())): {'$set': collection.find_one({'id': 'data'}, {'0': 1})}}},
                           upsert=True)
     collection = base['sets']
     collection.update_one(table,
